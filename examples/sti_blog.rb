ea7b605751047a9ce4768fcc844fa31f9f8886da
vhochstein <v.hochstein@highstone.de>
2011-02-21 10:35:33 +0100

    add example file for sti

--- examples/sti_blog.rb
+++ examples/sti_blog.rb
@@ -1,1 +1,24 @@
-das ist ein test
+require 'rubygems'
+require 'fileutils'
+
+puts "ActiveScaffold Department..."
+puts %x[rails g active_scaffold Department name:string]
+
+puts "ActiveScaffold Person.."
+puts %x[rails g active_scaffold Person name:string email:string balance:decimal report_to:integer department_id:integer]
+
+puts "ActiveScaffold Employee.."
+puts %x[rails g active_scaffold Employee]
+inject_into_file('app/models/employee.rb', '\0' + "  belongs_to :boss, :class_name => 'Manager' , :foreign_key => :report_to\n  belongs_to :department\n", /ActiveRecord::Base\n/)
+inject_into_file('app/models/employee.rb', 'Person', /ActiveRecord::Base/)
+
+puts "ActiveScaffold Manager.."
+puts %x[rails g active_scaffold Manager]
+inject_into_file('app/models/manager.rb', 'Employee', /ActiveRecord::Base/)
+
+puts "ActiveScaffold Customer.."
+puts %x[rails g active_scaffold Customer]
+inject_into_file('app/models/customer.rb', 'Person', /ActiveRecord::Base/)
+
+puts "Delete STI Child migrations.."
+Dir.glob("db/migrate/*create_{employees,managers,customers}.rb").each {|migration| FileUtils.rm(migration, :force => true) }
